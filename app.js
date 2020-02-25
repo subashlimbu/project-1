08bf7f400302d89601a0d4d459867d09bda104bd
subashlimbu <prajan_l@hotmail.com>
2020-02-25 12:18:50 +0000

    tuesday

--- app.js
+++ app.js
@@ -10,11 +10,13 @@ function setupGame() {
   let alienIndex = 0
   let bombIndex = 0
   let timerLaserId = 0
+  let computerIndex = null
   // let alien = 0
   // let aliensInRow = 20
   let alienArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70]
   // let alienArray = []
   let laserRepeat = null
+  let bombId = null
 
 
   for (let i = 0; i < gridCellCount; i++) {
@@ -30,9 +32,6 @@ function setupGame() {
     grid.appendChild(cell)
     cells.push(cell)
   }
-
-
-
 
   let alienDirection = 'right'
   let intervalId
@@ -99,86 +98,105 @@ function setupGame() {
       })
     }, 100)
   }
-  alienMoving()
 
   let laserUp = true
-  let laserIndex = player
   let missileIndex = 0
 
-  function fireLaser() {
-
-    timerLaserId = setInterval((laserIndex) => {
-      laserIndex = player - 20
-
-      if (laserIndex <= 390 && laserIndex >= 0) {
-        cells[laserIndex].classList.add('laser')
-        setTimeout(() => {
-          laserRepeat = setInterval(() => {
-            cells[laserIndex].classList.remove('laser')
-            laserIndex -= width
-            cells[laserIndex].classList.add('laser')
-            if (laserIndex <= 21 || alienArray.length === 0) {
-              clearInterval(laserRepeat)
-              cells[laserIndex].classList.remove('laser')
-            }
-          }, 200)
-        }, 200)
-      } 
-    }, 500)
+  function stopLaser() {
+    cells[laserIndex].classList.remove('laser')
+    clearInterval(timerLaserId)
+    laserIndex = null
   }
-  fireLaser()
 
-
-
-
-
-
+  function destroyComputer() {
+    const targetComputer = alienArray.indexOf(laserIndex)
+    cells[alienArray.slice(targetComputer, targetComputer + 1)].classList.remove('alien')
+    alienArray.splice(targetComputer, 1)
+    stopLaser()
+    if (alienArray.length === 0) {
+      // winGameSound.play()
+      // gameTheme.pause()
+      setTimeout(levelUp, 5000)
+    }
+  }
 
   function startBomb() {
     const randomComputerIndex = Math.floor(Math.random() * alienArray.length)
     // console.log(randomComputerIndex)
-    bombIndex = alienArray.slice(randomComputerIndex, randomComputerIndex + 1)
-    // timerBombId = setInterval(dropBomb, 1000)
-    timerBombId = setInterval(() => {
-      if (bombIndex > (width * width)) {
-        clearInterval(timerBombId)
-        bombIndex = 0
-        // startBomb()
-
-      } else {
-        clearInterval(timerBombId)
+    const a = alienArray.slice(randomComputerIndex, randomComputerIndex + 1)
+    bombIndex = parseInt(a)
+    // console.log(bombIndex)
+    const timerBombId = setInterval(() => {
+      if (bombIndex < (width * width)) {
         cells[bombIndex].classList.add('bomb')
-        setTimeout(removeBomb, 1000)
-        // loseLife()
+        setTimeout(() => {
+          bombId = setInterval(() => {
+            cells[bombIndex].classList.remove('bomb')
+            bombIndex += width
+            // console.log(bombIndex)
+            cells[bombIndex].classList.add('bomb')
+            if (bombIndex <= 21) {
+              clearInterval(laserRepeat)
+            }
+            startBomb()
+          }, 2000)
+        }, 2000)
+
       }
-    }, 100)
+    }, 2000)
   }
+ 
   startBomb()
 
 
-  function dropBomb() {
-    if (bombIndex > (width * width)) {
-      clearInterval(timerBombId)
-      bombIndex = 0
-      // startBomb()
-    } else {
-      clearInterval(timerBombId)
-      cells[bombIndex].classList.add('bomb')
-      setTimeout(removeBomb, 50)
-      // loseLife()
-    }
-  }
-  dropBomb()
+  // function dropBomb() {
+  //   if (bombIndex > (width * width)) {
+  //     clearInterval(timerBombId)
+  //     bombIndex = 0
+  //     startBomb()
+  //   } else {
+  //     clearInterval(timerBombId)
+  //     cells[bombIndex].classList.add('bomb')
+  //     setTimeout(removeBomb, 50)
+  //     // loseLife()
+  //   }
+  // }
+  // dropBomb()
 
-  function removeBomb() {
-    if (bombIndex <= (width * width))
-      cells[bombIndex].classList.remove('bomb')
-    bombIndex = +bombIndex + width
-    setTimeout(dropBomb, 500)
-  }
+  // function removeBomb() {
+  //   if (bombIndex <= (width * width)) {
+  //     console.log(bombIndex)
+  //     cells[bombIndex].classList.add('bomb')
+  //   }
+  //   const bombId = setInterval(() => {
+  //     cells[bombIndex].classList.remove('bomb')
+  //     bombIndex += width
+  //     console.log(bombIndex)
+  //     cells[bombIndex].classList.add('bomb')
+
+
+  //   }, 500)
+
+  // }
+
+  // removeBomb()
 
   document.addEventListener('keydown', (event) => {
     // console.log(event.key)
+
+    if (event.keyCode === 32) {
+      let laserIndex = player - width
+      const laserInterval = setInterval(() => {
+        if (laserIndex < 21) {
+          cells[laserIndex].classList.remove('laser')
+          clearInterval(laserInterval)
+        } else {
+          cells[laserIndex].classList.remove('laser')
+          laserIndex -= width
+          cells[laserIndex].classList.add('laser')
+        }
+      }, 200)
+    }
 
     if (event.key === 'ArrowRight') {
       if (player === cells.length - 1) {
@@ -193,20 +211,6 @@ function setupGame() {
       }
       cells[player].classList.remove('player')
       player -= 1
-      cells[player].classList.add('player')
-    } else if (event.key === 'ArrowUp') {
-      if (player < width) {
-        return
-      }
-      cells[player].classList.remove('player')
-      player -= width
-      cells[player].classList.add('player ')
-    } else if (event.key === 'ArrowDown') {
-      if (player > cells.length - width - 1) {
-        return
-      }
-      cells[player].classList.remove('player')
-      player += width
       cells[player].classList.add('player')
     }
   })
